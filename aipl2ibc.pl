b0468e70064ebcb122942171039034a359d3a490
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-08-25 01:21:43 -0400

    Update aipl2ibc.pl

--- aipl2ibc.pl
+++ aipl2ibc.pl
@@ -1,12 +1,23 @@
 # calculate inbreeding coefficients using AIPL genotypes
 # similar to VanRaden GRM 1 but allele frequency fixed to 0.5
 # missing genotypes excluded
+# use a pre-specified list of 0-based SNP indices for QC
 
 use strict;
 use warnings;
 
-@ARGV == 2 or die "Two arguments needed: aipl-geno-filename and output-filename\n";
-my ($aipl, $out) = @ARGV;
+@ARGV == 3 or die "Two arguments needed: aipl-geno-filename, output-filename, and kept-snp-list\n";
+my ($aipl, $out, $kept) = @ARGV;
+
+my @subset;
+open IN,$kept or die "Could not open $kept: $!\n";
+while(<IN>) {
+	if(/\d+/) {
+		push @subset, $&;
+	}
+}
+close IN;
+@subset = sort {$a <=> $b} @subset;
 
 my $num_processed = 0;
 open IN,$aipl or die "Could not open $aipl: $!\n";
@@ -23,7 +34,7 @@ while(<IN>)
 		$anim = $c[0];
 	}
 	
-	my @gg = split //,$c[-1];
+	my @gg = (split //,$c[-1])[@subset];
 	my $ibc = 0;
 	my $nm = 0;
 	for my $g (@gg) {
