d80b6cb9210ed6d688e27d333c73ba047fbf885a
Jicai Jiang <jiang18@terpmail.umd.edu>
2022-05-27 11:33:54 -0400

    Create edit_bim_alleles.pl

---
+++ edit_bim_alleles.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+
+@ARGV ==3 or die "3 files needed: coding-file, old-bim, new-bim\n";
+
+my ($coding, $bim1, $bim2) = @ARGV;
+
+my %pos_a1;
+my %pos_a2;
+open IN,$coding;
+$_=<IN>;
+while(<IN>) {
+	my @c =split /,/;
+	chomp $c[-1];
+	next if($c[-1] eq 'Not useful');
+	$pos_a1{$c[0]} = $c[2];
+	$pos_a2{$c[0]} = $c[4];
+}
+close IN;
+
+open IN,$bim1;
+open OUT,">$bim2";
+while(<IN>) {
+	chomp;
+	my @c =split /\s/;
+	if($c[4] eq '1' and $c[5] eq '2') {
+		$c[4] = $pos_a1{$c[3]};
+		$c[5] = $pos_a2{$c[3]};
+	} elsif ($c[4] eq '2' and $c[5] eq '1') {
+		$c[4] = $pos_a2{$c[3]};
+		$c[5] = $pos_a1{$c[3]};
+	} else {
+		print "Something unexpected: $_\n";
+	}
+	print OUT join "\t",@c;
+	print OUT "\n";
+}
+close IN;
+close OUT;
