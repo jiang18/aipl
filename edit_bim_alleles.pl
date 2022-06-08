d3fbce1d7d192683a3de7a41f74788e56cfb0c17
Jicai Jiang <jiang18@terpmail.umd.edu>
2022-06-08 15:25:04 -0400

    Update edit_bim_alleles.pl

--- edit_bim_alleles.pl
+++ edit_bim_alleles.pl
@@ -7,10 +7,11 @@ my ($coding, $bim1, $bim2) = @ARGV;
 
 my %pos_a1;
 my %pos_a2;
-open IN,$coding or die "Cannot open $coding: $!\n";
+open IN,$coding;
 $_=<IN>;
 while(<IN>) {
 	my @c =split /,/;
+  $c[-1] =~ s/\r//g;
 	chomp $c[-1];
 	next if($c[-1] eq 'Not useful');
 	$pos_a1{$c[0]} = $c[2];
@@ -18,22 +19,30 @@ while(<IN>) {
 }
 close IN;
 
-open IN,$bim1 or die "Cannot open $bim1: $!\n";
+open IN,$bim1;
 open OUT,">$bim2";
 while(<IN>) {
 	chomp;
 	my @c =split /\s+/;
-	if($c[4] eq '1' and $c[5] eq '2') {
-		$c[4] = $pos_a1{$c[3]};
-		$c[5] = $pos_a2{$c[3]};
-	} elsif ($c[4] eq '2' and $c[5] eq '1') {
-		$c[4] = $pos_a2{$c[3]};
-		$c[5] = $pos_a1{$c[3]};
-	} else {
-		print "Something unexpected: $_\n";
-	}
-	print OUT join "\t",@c;
-	print OUT "\n";
+ 
+  if(defined $pos_a1{$c[3]}) {
+  	if($c[4] eq '1') {
+  		$c[4] = $pos_a1{$c[3]};
+  	} elsif ($c[4] eq '2') {
+  		$c[4] = $pos_a2{$c[3]};
+  	}
+    if($c[5] eq '1') {
+  		$c[5] = $pos_a1{$c[3]};
+  	} elsif ($c[5] eq '2') {
+  		$c[5] = $pos_a2{$c[3]};
+  	}
+  	print OUT join "\t",@c;
+  	print OUT "\n";
+  } else {
+    print STDERR "Missing in $coding:\n    $_\n";
+    print OUT "$_\n";
+  }
 }
 close IN;
 close OUT;
+
